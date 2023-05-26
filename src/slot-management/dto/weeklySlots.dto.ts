e988ab7848e615f528214f8b3d1859171f20c507
Anthony Fink <40363736+psytonik@users.noreply.github.com>
2023-05-26 10:52:03 +0300

    adding information to schemas

--- src/slot-management/dto/weeklySlots.dto.ts
+++ src/slot-management/dto/weeklySlots.dto.ts
@@ -1,38 +1,63 @@
-import { IsArray, IsDate, IsNumber, IsString } from 'class-validator';
+import { IsArray, IsDate, IsNumber, IsString, MinDate } from 'class-validator';
 import { ApiProperty } from '@nestjs/swagger';
 import { Transform } from 'class-transformer';
+import { parseISO } from 'date-fns';
 
 export class WeeklySlotsDto {
-  @ApiProperty()
+  @ApiProperty({
+    type: Array,
+    description:
+      "must be array of strings 'Sunday, 'Monday','Tuesday','Wednesday','Thursday','Friday','Saturday' ",
+  })
   @IsArray()
   setWorkDays: string[];
 
-  @ApiProperty()
+  @ApiProperty({
+    type: Array,
+    description:
+      "must be array of strings 'Sunday, 'Monday','Tuesday','Wednesday','Thursday','Friday','Saturday' ",
+  })
   @IsArray()
   setHolidays: string[];
 
-  @ApiProperty()
+  @ApiProperty({
+    type: Number,
+    description: 'must be number of weeks',
+  })
   @IsNumber()
   weeksAhead: number;
 
-  @ApiProperty()
+  @ApiProperty({
+    type: String,
+    description: 'format of opening time must be for example 09:00',
+  })
   @IsString()
   openingHours: string;
 
-  @ApiProperty()
+  @ApiProperty({
+    type: String,
+    description: 'format of closing hour must be for example 22:00',
+  })
   @IsString()
   closingHours: string;
 
-  @ApiProperty()
+  @ApiProperty({
+    type: String,
+    description: '10 min or 15 min or 60 min or 75 min etc',
+  })
   @IsString()
   lunchDuration: string;
 
-  @ApiProperty()
+  @ApiProperty({
+    type: String,
+    description: '10 min or 15 min or 60 min or 75 min etc',
+  })
   @IsString()
   timePerClient: string;
 
-  @ApiProperty()
+  @ApiProperty({ type: String, description: 'Date in ISO format (yyyy-mm-dd)' })
   @IsDate()
-  @Transform(({ value }) => new Date(value))
+  @MinDate(new Date(), { message: 'Start date cannot be in the past.' })
+  @Transform(({ value }) => parseISO(value), { toClassOnly: true })
   startDate: Date;
 }
