bd2ab6543b00e20716886e57dac65c7a49f671f3
Anthony Fink <40363736+psytonik@users.noreply.github.com>
2023-06-21 13:34:59 +0300

    adding compression to main app

--- src/main.ts
+++ src/main.ts
@@ -3,12 +3,14 @@ import { AppModule } from './app.module';
 import { INestApplication, ValidationPipe } from '@nestjs/common';
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import { ConfigService } from '@nestjs/config';
+import * as compression from 'compression';
 
 const configService = new ConfigService();
 const PORT = configService.get('APP_PORT');
 async function bootstrap() {
   const app: INestApplication = await NestFactory.create(AppModule);
   app.enableCors();
+  app.use(compression());
   app.useGlobalPipes(new ValidationPipe());
   const config = new DocumentBuilder()
     .setTitle('Smart Booking Api')
